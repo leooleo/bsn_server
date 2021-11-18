2b00a624a4076595c533951103bda41f9a4b9198
Carlos Eduardo <cadutaborda@gmail.com>
2021-11-17 23:16:04 -0300

    Updated tests

--- cypress/integration/covid19/index.js
+++ cypress/integration/covid19/index.js
@@ -1,14 +1,13 @@
 import {Given, And, Then} from "cypress-cucumber-preprocessor/steps"
-const { visit, the } = cy;
 
 Given("that I am on the Vital Signs Page", () => {
-    visit('/') 
+    cy.visit('/') 
     cy.contains('Vital Signs Monitor').click()
     cy.url().should('include', '/monitor')
 });
 When("any of the patients show a negative change in any of the covid-19 related vital signs", () => {
-    cy.contains(/([0-9]).+%/)
-    cy.contains(/([0-9]).+C/)
+    cy.contains(/([0-9])+.([0-9])%/)
+    cy.contains(/([0-9])+.([0-9]).+C/)
 });
 Then("I should see an covid-19 alert triggering in the patient session", () => {
     cy.contains("Covid Risk")
